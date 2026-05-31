c99fc0632b813c91c4e9dcaf40e6d165034413e4
User12345PritamDey <pdey82662@gmail.com>
2026-05-31 23:47:47 +0530

    add

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');//import the mongoose driver to connect the Node.js and MongoDB
 //const MongoURL = 'mongodb://localhost:27017/myseconddatabase';//import the local MongoDB URL
-const MongoURL = 'mongodb://hellopritamdey:hellopritam12345dey@ac-ipa5bsb-shard-00-00.e2m0r1t.mongodb.net:27017,ac-ipa5bsb-shard-00-01.e2m0r1t.mongodb.net:27017,ac-ipa5bsb-shard-00-02.e2m0r1t.mongodb.net:27017/myseconddatabase?ssl=true&replicaSet=atlas-nfovpl-shard-0&authSource=admin&appName=Cluster0'
+require('dotenv').config();
+const MongoURL = process.env.ONLINE_URL;
 mongoose.connect(MongoURL);//connect the URL with mongodb
 const db = mongoose.connection;
 db.on('connected',()=>{
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const db = require('./db.js');
+require('dotenv').config();
+const PORT = process.env.PORT||3000;
 const BodyParser = require('body-parser');
 app.use(BodyParser.json());
 const student = require('./routes/student_router.js');
 app.use('/student',student);
-app.listen(7000,()=>{
+app.listen(PORT,()=>{
     console.log("This is your port");
 })
+
 
